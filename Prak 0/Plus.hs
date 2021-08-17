40de4ed3db5e6c4d3afef656366f3158d06e9cd9
Steven Wen <73114262+StevenWen81@users.noreply.github.com>
2021-08-17 12:50:01 +0700

    Add files via upload

---
+++ Prak 0/Plus.hs
@@ -0,0 +1,9 @@
+-- NIM/Nama     : 16520203/Steven
+-- Nama file    : DeretSegitiga.hs
+-- Topik        : Praktikum 0 Soal Kedua
+-- Tanggal      : 5 Feb 2021
+-- Deskripsi    : Mengoutputkan jumlah dari 2 angka
+
+module Plus where
+    plus :: Int -> Int -> Int
+    plus x y = x + y
---
+++ Prak 0/hello.hs
@@ -0,0 +1,9 @@
+-- NIM/Nama     : 16520203/Steven
+-- Nama file    : DeretSegitiga.hs
+-- Topik        : Praktikum 0 Soal Pertama
+-- Tanggal      : 5 Feb 2021
+-- Deskripsi    : Mengoutputkan kata "Hello, World!"
+
+module Main where
+    main :: IO()
+    main = putStrLn "Hello, World!"
