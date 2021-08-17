656cb9bf6eb200a04caa9bc1d66df3e38d4286fe
Steven Wen <73114262+StevenWen81@users.noreply.github.com>
2021-08-17 13:04:13 +0700

    Add files via upload

---
+++ prak_pengenalan_python/Test1_16520203.py
@@ -0,0 +1,13 @@
+# Steven
+# 16520203
+# 18 Maret 2021
+
+# DESKIRPSI
+# Mengoutputkan kalimat "Hello, World!"
+
+# KAMUS
+# output = variabel bertipe string
+
+# ALGORITMA
+output = "Hello, World!"
+print(output)
---
+++ prak_pengenalan_python/jml3bil.py
@@ -0,0 +1,19 @@
+# Steven
+# 16520203
+# 18 Maret 2021
+
+# DESKRIPSI
+# Menerima 3 output berupa integer, dan menuliskan hasil penjumlahannya
+
+# KAMUS
+# bil_satu, bil_dua, bil_tiga = variabel berupa integer
+# jumlah_bil = variabel berupa integer
+
+# ALGORITMA
+bil_satu = int(input())
+bil_dua = int(input())
+bil_tiga = int(input())
+
+jumlah_bil = bil_satu + bil_dua + bil_tiga
+
+print(jumlah_bil)
